39c29d915190785e8b5911d3c3005d29fc2cb5c5
jaschalk <56085146+jaschalk@users.noreply.github.com>
2020-02-01 09:49:28 -0500

    Started building the Trick class

--- GameObjects/Trick.py
+++ GameObjects/Trick.py
@@ -1,4 +1,16 @@
+from GameObjects import Player
+from GameObjects import FailCard
+from GameObjects import TrumpCard
 class Trick:
+
+    parentGame = None
+    suitLead = None
+    winnningPlayer = None
+    containedCards = []
 
     def __init__(self):
         pass
+
+    def accept(self, aCard):
+        if self.winnningPlayer is not None:
+            pass
