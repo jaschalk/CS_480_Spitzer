672656d2348d98c5259c396c8419f8794a285fbe
AnnaMarie1714 <amdamico17@gmail.com>
2020-02-02 14:11:53 -0500

    Fix names for variables AD

--- HandTest.py
+++ HandTest.py
@@ -4,22 +4,22 @@ from GameObjects import *
 class HandTest(unittest.TestCase):
 
     def setUp(self):
-        self.testHand = Hand() #How to fix this error
+        self.testHand = Hand()
 
     def test_on_init(self):
         self.assertEqual(self.testHand.cardsInHand.size(), 8)
         #How to assert that a valid play list exists and is initialized to true?
 
     def test_card_played(self):
-        startSize = self.hand.cardsInHand.size()
-        self.hand.playCard() #should this have anything passed in?
-        self.assertEqual(self.hand.cardsInHand.size(), startSize - 1)
-        self.hand.updateValidPlay()
+        startSize = self.testHand.cardsInHand.size()
+        self.testHand.playCard() #should this have anything passed in?
+        self.assertEqual(self.testHand.cardsInHand.size(), startSize - 1)
+        self.testHand.updateValidPlay()
         #How to assert that play list has been updated properly?
 
     def tearDown(self):
-        self.assertEqual(self.hand.cardsInHand.size(), 0)
-        del self.hand
+        self.assertEqual(self.testHand.cardsInHand.size(), 0)
+        del self.testHand
 
 if __name__ == "__main__":
     unittest.main()
