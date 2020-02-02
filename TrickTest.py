9e772f31da89f27f3d8d40ad8e25c2e4c7a4067f
jaschalk <56085146+jaschalk@users.noreply.github.com>
2020-02-02 15:31:10 -0500

    Expanded test case for the trick accepting cards.

--- TrickTest.py
+++ TrickTest.py
@@ -9,7 +9,7 @@ class TrickTest(unittest.TestCase):
         self.tempPlayers = [Player(0), Player(1), Player(2), Player(3)]
         self.testTrick.parentRound.playerList = self.tempPlayers
         for i in range(4):
-            self.tempPlayers[i].accept(FailCard(9+i, "hearts"))
+            self.tempPlayers[i].accept(FailCard(10+i, "hearts"))
 
     def test_on_init(self):
         self.assertEqual(self.testTrick.cardsInPlay.size(), 0)
@@ -20,17 +20,28 @@ class TrickTest(unittest.TestCase):
         self.assertEqual(self.testTrick.containedPoints, 0)
 
     def test_on_accept_card(self):
-        self.tempCard1 = FailCard(12,"hearts")
-        self.testTrick.accept(self.tempCard1)
-        self.assertEqual(self.testTrick.cardsInPlay.size(), 1)
-        self.tempCard2 = FailCard(11,"hearts")
+        self.testTrick.accept(self.tempPlayers[0].hand.play_card(0))
+        self.assertEqual(self.testTrick.playedCards.size(), 1)
+        self.assertEqual(self.testTrick.suitlead, "hearts")
+        self.assertEqual(self.testTrick.winningPlayer, self.tempPlayers[0])
+        self.testTrick.accept(self.tempPlayers[1].hand.play_card(0))
+        self.assertEqual(self.testTrick.playedCards.size(), 2)
+        self.assertEqual(self.testTrick.suitlead, "hearts")
+        self.assertEqual(self.testTrick.winningPlayer, self.tempPlayers[0])
+        self.tempPlayers[2].accept(TrumpCard(0))
+        self.testTrick.accept(self.tempPlayers[2].hand.play_card(1))
+        self.assertEqual(self.testTrick.winningPlayer, self.tempPlayers[2])
 
     def test_on_fill(self):
         for i in range(4):
-            self.testTrick.accept(self.tempPlayers[i].cards[0])
+            self.testTrick.accept(self.tempPlayers[i].hand.cards[0])
+        self.assertIsNone(self.testTrick)
 
 
     def tearDown(self):
+        for player in self.tempPlayers:
+            del player
+        del self.tempPlayers
         del self.testTrick.parentRound
         del self.testTrick
 
