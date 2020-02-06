2e3678709487ca93363b8c60b2c3031dbf7b6caa
AnnaMarie1714 <amdamico17@gmail.com>
2020-02-05 20:04:59 -0500

    Began Game test. Questions to come. AD

---
+++ GameTest.py
@@ -0,0 +1,13 @@
+import unittest
+from GameObjects import *
+
+class GameTest(unittest.TestCase):
+
+    def setUp(self):
+       self.testGame = Game()
+
+    def test_on_init(self):
+        
+
+    if __name__ == "__main__":
+        unittest.main()
