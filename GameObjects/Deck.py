e4822d83fbe73071efa34727235c409ddda38e0d
Jesse Schalk <jschalk@nmu.edu>
2020-02-10 18:26:56 -0500

    Refactored the way the deck gets cards added to it.

--- GameObjects/Deck.py
+++ GameObjects/Deck.py
@@ -5,7 +5,7 @@ class Deck:
     '''
     The Deck class is used to hold onto, and distribute, the cards at the start of rounds.
     '''
-    cardlist = []
+    card_list = []
     
     def __init__(self):
         self.populate_deck()
@@ -14,19 +14,15 @@ class Deck:
         '''
         Populating the deck will clear the contents of the cardlist and refill it with the proper collection of cards to play the game with.
         '''
-        for i in range(14): #highest rank is expected to be 0 and lowest 13
-            if i < 8:
-                self.cardlist.append(Card(i, "trump"))
-            elif i != 13:
-                self.cardlist.append(Card(i, "trump"))
-                self.cardlist.append(Card(i, "spades"))
-                self.cardlist.append(Card(i, "hearts"))
-                self.cardlist.append(Card(i, "clubs"))
-            else:
-                self.cardlist.append(Card(i, "spades"))
-                self.cardlist.append(Card(i, "hearts"))
-                self.cardlist.append(Card(i, "clubs"))
-        shuffle(self.cardlist)
+        for i in range(15):
+            self.card_list.append(Card(i, "trump"))
+        for i in range(9,15):
+            self.card_list.append(Card(i, "clubs"))
+        for i in range(9,15):
+            self.card_list.append(Card(i, "spades"))
+        for i in range(9,15):
+            self.card_list.append(Card(i, "hearts"))
+        shuffle(self.card_list)
 
     def deal_cards_to(self, targetPlayer):
         '''
