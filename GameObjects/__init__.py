256390f2e08df43fd6aff37d646b98270c3e57af
Jesse Schalk <jschalk@nmu.edu>
2020-02-10 17:48:30 -0500

    Updated the GameObjects package initialization file.

--- GameObjects/__init__.py
+++ GameObjects/__init__.py
@@ -3,11 +3,3 @@ This package contains the classes used by the card game framework.
 Note that "import *" will create namespace conflicts when used in
 the class definitions themselves.
 '''
-from GameObjects import *
-import Card
-import Deck
-import Game
-import Hand
-import Trick
-import Round
-import Player
