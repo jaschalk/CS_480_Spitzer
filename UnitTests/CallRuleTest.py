1d0163995043d056d56830e876702804a153236c
Jesse Schalk <jschalk@nmu.edu>
2020-02-11 22:35:09 -0500

    Added frame work for the call rule tests.

---
+++ UnitTests/CallRuleTest.py
@@ -0,0 +1,15 @@
+import unittest
+from GameObjects import Card
+from GameObjects import Hand
+from GameObjects import Player
+
+class CallRuleTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
