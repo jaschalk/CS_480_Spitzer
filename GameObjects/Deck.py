107026cf516aca1c5a685c0fdbdb859b06ab456e
jaschalk <56085146+jaschalk@users.noreply.github.com>
2020-02-12 17:19:48 -0500

    Fixed syntax errors related to creating cards.

--- GameObjects/Deck.py
+++ GameObjects/Deck.py
@@ -15,13 +15,13 @@ class Deck:
         Populating the deck will clear the contents of the cardlist and refill it with the proper collection of cards to play the game with.
         '''
         for i in range(15):
-            self.card_list.append(Card(i, "trump"))
+            self.card_list.append(Card.Card(i, "trump"))
         for i in range(9,15):
-            self.card_list.append(Card(i, "clubs"))
+            self.card_list.append(Card.Card(i, "clubs"))
         for i in range(9,15):
-            self.card_list.append(Card(i, "spades"))
+            self.card_list.append(Card.Card(i, "spades"))
         for i in range(9,15):
-            self.card_list.append(Card(i, "hearts"))
+            self.card_list.append(Card.Card(i, "hearts"))
         shuffle(self.card_list)
 
     def deal_cards_to(self, targetPlayer):
