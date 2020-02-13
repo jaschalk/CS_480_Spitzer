ea2456598ea4d38c89d55ab862ef2987a5ebe456
jaschalk <56085146+jaschalk@users.noreply.github.com>
2020-02-13 14:43:11 -0500

    Fixed some naming issues in the trick class.

--- GameObjects/Trick.py
+++ GameObjects/Trick.py
@@ -8,22 +8,22 @@ class Trick:
     winning_card = None
     leading_player = None
     points_on_trick = 0
-    played_cards = []
+    played_cards_list = []
 
     def __init__(self, aRound, aPlayer):
         self.parent_round = aRound
         self.leading_player = aPlayer
 
     def accept(self, aCard):
-       if suit_lead is None:
-           suit_lead = aCard.get_suit()
+       if self.suit_lead is None:
+           self.suit_lead = aCard.get_suit()
            self.winning_card = aCard
        else:
            if self.winning_card.accept(aCard) is False:
                self.winning_player = aCard.get_owning_player()
                self.winning_card = aCard
-       self.played_cards.append(aCard)
+       self.played_cards_list.append(aCard)
        self.points_on_trick += aCard.get_point_value()
-       if len(self.played_cards) == 4:#the trick is now done, process accordingly
-           self.parent_round.on_trick_end(self.winning_player, self.points_on_trick, self.played_cards)
+       if len(self.played_cards_list) == 4:#the trick is now done, process accordingly
+           self.parent_round.on_trick_end(self.winning_player, self.points_on_trick, self.played_cards_list)
 
