a04291aa0afa59b74af42629a84ebab03d2c4b65
jaschalk <56085146+jaschalk@users.noreply.github.com>
2020-02-13 17:29:01 -0500

    Revised the trick to store cards played in the order of the players, rather than of play.

--- GameObjects/Trick.py
+++ GameObjects/Trick.py
@@ -8,7 +8,7 @@ class Trick:
     winning_card = None
     leading_player = None
     points_on_trick = 0
-    played_cards_list = []
+    played_cards_list = [None, None, None, None]
 
     def __init__(self, aRound, aPlayer):
         self.parent_round = aRound
@@ -22,7 +22,7 @@ class Trick:
            if self.winning_card.accept(aCard) is False:
                self.winning_player = aCard.get_owning_player()
                self.winning_card = aCard
-       self.played_cards_list.append(aCard)
+       self.played_cards_list[aCard.get_owning_player()] = aCard #don't append the cards, replace at the player index to preserve play order
        self.points_on_trick += aCard.get_point_value()
        if len(self.played_cards_list) == 4:#the trick is now done, process accordingly
            self.parent_round.on_trick_end(self.winning_player, self.points_on_trick, self.played_cards_list)
