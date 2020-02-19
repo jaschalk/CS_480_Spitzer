088e7d24fe691d94717d2f763d5e5012c69589c5
Jesse Schalk <jschalk@nmu.edu>
2020-02-18 23:03:08 -0500

    Changed some naming in the deck and trick.

--- GameObjects/Deck.py
+++ GameObjects/Deck.py
@@ -5,7 +5,7 @@ class Deck:
     '''
     The Deck class is used to hold onto, and distribute, the cards at the start of rounds.
     '''
-    card_list = []
+    _card_list = []
     
     def __init__(self):
         self.populate_deck()
@@ -15,19 +15,19 @@ class Deck:
         Populating the deck will clear the contents of the cardlist and refill it with the proper collection of cards to play the game with.
         '''
         for i in range(15):
-            self.card_list.append(Card.Card(i, "trump"))
+            self._card_list.append(Card.Card(i, "trump"))
         for i in range(9,15):
-            self.card_list.append(Card.Card(i, "clubs"))
+            self._card_list.append(Card.Card(i, "clubs"))
         for i in range(9,15):
-            self.card_list.append(Card.Card(i, "spades"))
+            self._card_list.append(Card.Card(i, "spades"))
         for i in range(9,15):
-            self.card_list.append(Card.Card(i, "hearts"))
-        shuffle(self.card_list)
+            self._card_list.append(Card.Card(i, "hearts"))
+        shuffle(self._card_list)
 
     def deal_cards_to(self, targetPlayer):
         '''
         This method will cause 8 cards to visit the targetPlayer. 
         '''
         for i in range(8):
-            targetPlayer.accept(self.card_list.pop())
+            targetPlayer.accept(self._card_list.pop())
             
