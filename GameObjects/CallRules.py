d0e3ed8b43ea2442e51a41335e869540074013b9
jaschalk <56085146+jaschalk@users.noreply.github.com>
2020-02-19 10:06:32 -0500

    Changed the stored values in the call rules call filter.

--- GameObjects/CallRules.py
+++ GameObjects/CallRules.py
@@ -2,10 +2,10 @@ class CallRules:
 
     def __init__(self): #setup singleton behavior for the rules
         #calls: NC, AC, AS, AH, FT, Z, ZS, ZSS
-        self._call_filter = [(1,1<<14), #remember to ~ the hand first
-                            (2,1<<20), #remember to ~ the hand first
-                            (3,1<<26), #remember to ~ the hand first
-                            (4,1<<14 + 1<<20 + 1<<26)]
+        self._call_filter = [(2,1<<14), #remember to ~ the hand first
+                            (4,1<<20), #remember to ~ the hand first
+                            (8,1<<26), #remember to ~ the hand first
+                            (16,1<<14 + 1<<20 + 1<<26)]
 
     def validate(self, a_hand): #should the hand store it's numerical representation, or should it be calculated here?
         valid_call_state = 225 #this is the decimal value representing that any player can make NC, Z, ZS, ZSS calls
@@ -13,9 +13,9 @@ class CallRules:
         for call, value in self._call_filter:
             if call == 4:
                 if hand_numerical_state & value == value:
-                    valid_call_state += 1<<call
+                    valid_call_state += call
             else:
                 if ~hand_numerical_state & value == value:
-                    valid_call_state += 1<<call
+                    valid_call_state += call
 
         return [int(d) for d in bin(valid_call_state)[2:]][::-1]
