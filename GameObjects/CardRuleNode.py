a1da292eebd8148776f35feb6ddc4d88e11bd4df
Jesse Schalk <jschalk@nmu.edu>
2020-02-21 10:20:18 -0500

    Added some comments describing the card rule node behavior.

--- GameObjects/CardRuleNode.py
+++ GameObjects/CardRuleNode.py
@@ -4,17 +4,17 @@ class CardRuleNode:
 
     _left = None
     _right = None
-    _condition = None
+    _evaluator_function = None
     _parent_tree = None
 
     def __init__(self, parent, left_node, right_node, code_block):
         self._parent_tree = parent
         self._left = left_node
         self._right = right_node
-        self._condition = code_block
+        self._evaluator_function = code_block #this should always be a function that returns True/False
 
-    def validate(self, *args): #this isn't quite what I want
-        if self._condition(*args): #I'm not sure how to handle this behavior
+    def validate(self, *args): #this might work?
+        if self._evaluator_function(*args):
             return self._left.validate(*args)
         else:
             return self._right.validate(*args)
@@ -26,4 +26,5 @@ if __name__ == "__main__":
         if a < b:
             return True
     test = CardRuleNode(None, true_node, false_node, sample)
-    print(test.validate(True))
+    print(test.validate((1,2))) #since the sample function takes 2 parameters a tuple containing those parameters
+                                #must be passed into the validate method
