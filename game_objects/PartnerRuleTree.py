f5c368cc0e65fc11bf41b9ec8b2c8916d59bb167
AnnaMarie1714 <amdamico17@gmail.com>
2020-02-24 12:57:40 -0500

    Started partner rule tree AD

--- game_objects/PartnerRuleTree.py
+++ game_objects/PartnerRuleTree.py
@@ -2,5 +2,10 @@ from game_objects import RuleNode
 
 class PartnerRuleTree:
 
+    _root = None
+
+    def get_root(self):
+        return self._root
+
     def __init__(self):
         self.root = PartnerRuleNode
