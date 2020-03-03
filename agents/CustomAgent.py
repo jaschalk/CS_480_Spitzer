07af37193f728711666995559825164e0c6adfda
Jesse Schalk <jschalk@nmu.edu>
2020-03-02 19:42:07 -0500

    Small update to the round and the custom agent.

--- agents/CustomAgent.py
+++ agents/CustomAgent.py
@@ -1,9 +1,14 @@
 from agents.agent import Agent
 
 class CustomAgent(Agent):
+
+    def estimate_strength_of_hand(self, cards_in_hand_list):
+        pass
     
-    def make_call(self, game_state):
+    def make_call(self, cards_in_hand_list):
+        
         pass
 
     def play_card(self, game_state):
+        #game_state is expected to be a dict containing the trick history, trick point history, call matrix, current trick, and the current player
         pass
